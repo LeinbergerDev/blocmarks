f000d0000d8bf6010d4c0b83443b16cb61ca8d73
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-06-27 16:20:27 -0500

    add email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -14,18 +14,18 @@ class IncomingController < ApplicationController
     # user_object = User.find_by email: user
       @user = User.find_by(email: params[:user])
 
-      # if @user
-      #   @topic = Topic.find_by(title: topic)
-      #   @topic.user_id = @user.id
-      #   if @topic
-      #     @bookmark = Topic.bookmark.create(url: body, @topic.id)
-      #     @topic.save
-      #     else
-      #     @topic = Topic.new(title: topic)
-      #     @bookmark = @topic.bookmark.create(url: body, @topic.id)
-      #   end
+      if @user
+        @topic = Topic.find_by(title: topic)
+        @topic.user_id = @user.id
+        if @topic
+          @bookmark = Topic.bookmark.create(url: body, @topic.id)
+          @topic.save
+          else
+          @topic = Topic.new(title: topic)
+          @bookmark = @topic.bookmark.create(url: body, @topic.id)
+        end
         
-      # end
+      end
 
     head 200
   end
