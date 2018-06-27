0c3d991ddf0e1549470958b4c42f952be8dc3829
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-06-27 14:44:46 -0500

    parse test

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,6 +4,12 @@ class IncomingController < ApplicationController
   def create
     puts "INCOMING PARAMS HERE: #{params}"
 
+    user = params[:user]
+    puts "#{user}"
+    topic = params[:subject]
+    puts "#{topic}"
+    body = params["body-plain"]
+    puts "#{body}"
     head 200
   end
 end
