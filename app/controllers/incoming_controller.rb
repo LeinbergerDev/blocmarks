d47254eee7211dc5e1a5192e11fd6a08cc1cf963
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-06-27 15:33:19 -0500

    add bookmark from email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -12,7 +12,20 @@ class IncomingController < ApplicationController
     puts "#{body}"
 
     # user_object = User.find_by email: user
+      @user = User.find_by(email: params[:user])
 
+      # if @user
+      #   @topic = Topic.find_by(title: topic)
+      #   @topic.user_id = @user.id
+      #   if @topic
+      #     @bookmark = Topic.bookmark.create(url: body, @topic.id)
+      #     @topic.save
+      #     else
+      #     @topic = Topic.new(title: topic)
+      #     @bookmark = @topic.bookmark.create(url: body, @topic.id)
+      #   end
+        
+      # end
     # if !user_object
 
     # topic_object = Topic.find_by title: topic
