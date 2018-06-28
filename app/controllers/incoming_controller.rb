1aac650550f703c9150408cecb592cc529c7673b
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-06-27 20:31:11 -0500

    add email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,20 +4,20 @@ class IncomingController < ApplicationController
   def create
     puts "INCOMING PARAMS HERE: #{params}"
 
-    user = params[:sender]
+    email = params[:sender]
     puts "#{user}"
     topic = params[:subject]
     puts "#{topic}"
     body = params["body-plain"]
     puts "#{body}"
 
-    # user_object = User.find_by email: user
-      user_object = User.find_by(email: user.downcase!)
-      if !user_object 
+    # user = User.find_by email: user
+      user = User.find_by(email: email.downcase!)
+      if !user 
         puts "user not found Create user"
-        user_object = User.new(email: user, password: 'PleaseChangePassword', password_confirmation: 'PleaseChangePassword')
-        user_object.save
-        user_object = User.find_by(email: user.downcase!)
+        user = User.new(email: email.downcase!, password: 'PleaseChangePassword', password_confirmation: 'PleaseChangePassword')
+        user.save
+        user = User.find_by(email: email.downcase!)
       end
       topic_object = Topic.find_by(title: topic)
       if topic_object
@@ -26,8 +26,8 @@ class IncomingController < ApplicationController
         bookmark.save
         else
         puts "no topic found"
-        topic_object = Topic.create!(title: topic, user_id: user_object.id)
-        # topic_object.user_id = user_object.id
+        topic_object = Topic.create!(title: topic, user_id: user.id)
+        # topic_object.user_id = user.id
         topic_object.save
         bookmark = topic_object.bookmarks.create(url: body, topic_id: topic_object.id)
         bookmark.save
