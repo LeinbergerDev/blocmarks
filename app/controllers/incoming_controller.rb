5fafd16685a1b2dc2b1307b9cc8a9682b8c2c63b
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-06-27 19:13:12 -0500

    add email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -13,25 +13,22 @@ class IncomingController < ApplicationController
 
     # user_object = User.find_by email: user
       user_object = User.find_by(email: user.downcase!)
-      puts "#{user_object.email}"
-      if user_object
-        puts "user found"
-        topic_object = Topic.find_by(title: topic)
-        if topic_object
-          puts "topic found"
-          bookmark = topic_object.bookmarks.create(url: body, topic_id: topic_object.id)
-          bookmark.save
-          else
-          puts "no topic found"
-          topic_object = Topic.create!(title: topic, user_id: user_object.id)
-          # topic_object.user_id = user_object.id
-          topic_object.save
-          bookmark = topic_object.bookmarks.create(url: body, topic_id: topic_object.id)
-          bookmark.save
-        end
-        puts "user not found"
+      if !user_object
+        user_object = User.create(email: user.downcase!, password: 'PleaseChangePassword')
       end
-
+      topic_object = Topic.find_by(title: topic)
+      if topic_object
+        puts "topic found"
+        bookmark = topic_object.bookmarks.create(url: body, topic_id: topic_object.id)
+        bookmark.save
+        else
+        puts "no topic found"
+        topic_object = Topic.create!(title: topic, user_id: user_object.id)
+        # topic_object.user_id = user_object.id
+        topic_object.save
+        bookmark = topic_object.bookmarks.create(url: body, topic_id: topic_object.id)
+        bookmark.save
+      end
     head 200
   end
 end
